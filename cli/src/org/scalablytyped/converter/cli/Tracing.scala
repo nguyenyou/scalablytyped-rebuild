da28e4b0b5922c6d18280b976e4addfbf8d7888d
nguyenyou <tunt081295@gmail.com>
2025-08-31 13:55:48 +0700

    package.json

--- cli/src/org/scalablytyped/converter/cli/Tracing.scala
+++ cli/src/org/scalablytyped/converter/cli/Tracing.scala
@@ -1,5 +1,8 @@
 package org.scalablytyped.converter.cli
 
+import org.scalablytyped.converter.internal.Json
+import org.scalablytyped.converter.internal.ts.PackageJson
+
 object Tracing {
   private val inDirectory = os.pwd
   lazy val paths = new Paths(inDirectory)
@@ -9,6 +12,9 @@ object Tracing {
   def mainNoExit(args: Array[String]): Int = {
     val packageJsonPath = paths.packageJson.getOrElse(sys.error(s"${inDirectory} does not contain package.json"))
     val nodeModulesPath = paths.node_modules.getOrElse(sys.error(s"${inDirectory} does not contain node_modules"))
+    val packageJson = Json.force[PackageJson](packageJsonPath)
+    println(packageJson)
+    
     println(packageJsonPath)
     println(nodeModulesPath)
     println("Hello, world!")
